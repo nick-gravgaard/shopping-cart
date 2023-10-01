7fa561416b1a691a5e96fe4028d544d42c1c3287
Nick Gravgaard <nick@nick-gravgaard.com>
2023-10-01 15:01:03 +0100

    Add Product enum

--- src/ShoppingCart.scala
+++ src/ShoppingCart.scala
@@ -1,4 +1,7 @@
 object ShoppingCart:
+  enum Product(val price: BigDecimal):
+    case Apple extends Product(BigDecimal(0.60))
+    case Orange extends Product(BigDecimal(0.25))
 
   def main(args: Array[String]): Unit =
     println(generateReceipt(args))
